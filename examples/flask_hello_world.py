d1ece2c09e5e062ce3c4c56b3be081b717c1afd9
leondutoit <leon@comoyo.com>
2015-01-15 17:01:07 +0100

    more fixes

--- examples/flask_hello_world.py
+++ examples/flask_hello_world.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/sayhello")
 def hello():
     return "Hello World"
 
 if __name__ == '__main__':
-    app.run(port = 9009)
+    app.run(port = 9009, debug = True)
